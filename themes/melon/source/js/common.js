22596b12fad81dae13d019f602b6c33c670cb438
pll513 <pu_af@gzhc365.com>
2017-09-25 23:47:46 +0800

    5th commit

---
+++ themes/melon/source/js/common.js
@@ -0,0 +1,16 @@
+(function(w, d){
+  
+  var btnNavbarToggle = d.getElementById('navbar-toggle'),
+    mobileNav = d.getElementById('mobile-nav');
+  btnNavbarToggle.addEventListener('click', function (e) {
+    var target = e.target;
+    if (~target.className.indexOf('active')) {
+      target.className = target.className.replace(/\s*active\s*/, '');
+      mobileNav.className += ' none';
+    } else {
+      target.className += ' active';
+      mobileNav.className = mobileNav.className.replace(/\s*none\s*/, '')
+    }
+  });
+  
+})(window, document);
